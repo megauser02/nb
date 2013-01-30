6c2fb88bca34b3cee6ed0de8f1bedebf6982efa7
Mega  User  II <megauser02@hotmail.com>
2013-01-30 11:21:45 -0600

    Act v01.01
    Test, nuevo background, renombrar CSS

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/nb/admin/');
+define('HTTP_CATALOG', 'http://localhost/nb/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/nb/admin/');
+define('HTTPS_CATALOG', 'http://localhost/nb/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\wamp\www\nb/admin/');
+define('DIR_SYSTEM', 'C:\wamp\www\nb/system/');
+define('DIR_DATABASE', 'C:\wamp\www\nb/system/database/');
+define('DIR_LANGUAGE', 'C:\wamp\www\nb/admin/language/');
+define('DIR_TEMPLATE', 'C:\wamp\www\nb/admin/view/template/');
+define('DIR_CONFIG', 'C:\wamp\www\nb/system/config/');
+define('DIR_IMAGE', 'C:\wamp\www\nb/image/');
+define('DIR_CACHE', 'C:\wamp\www\nb/system/cache/');
+define('DIR_DOWNLOAD', 'C:\wamp\www\nb/download/');
+define('DIR_LOGS', 'C:\wamp\www\nb/system/logs/');
+define('DIR_CATALOG', 'C:\wamp\www\nb/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'nb');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/nb/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/nb/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\wamp\www\nb/catalog/');
+define('DIR_SYSTEM', 'C:\wamp\www\nb/system/');
+define('DIR_DATABASE', 'C:\wamp\www\nb/system/database/');
+define('DIR_LANGUAGE', 'C:\wamp\www\nb/catalog/language/');
+define('DIR_TEMPLATE', 'C:\wamp\www\nb/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\wamp\www\nb/system/config/');
+define('DIR_IMAGE', 'C:\wamp\www\nb/image/');
+define('DIR_CACHE', 'C:\wamp\www\nb/system/cache/');
+define('DIR_DOWNLOAD', 'C:\wamp\www\nb/download/');
+define('DIR_LOGS', 'C:\wamp\www\nb/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'nb');
+define('DB_PREFIX', 'oc_');
+?>
